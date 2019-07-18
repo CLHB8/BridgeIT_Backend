01102819d9352371d5aaa55bf1073f12d8c6d3e9
NDKN12 <Neurei95>
2019-07-18 19:42:26 +0200

    Implementing entire Rating Service (backend-side)

---
+++ src/routes/ratings.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express  = require('express');
+const router   = express.Router();
+
+const middlewares    = require('../middlewares');
+const RatingsController = require('../controllers/ratings')
+
+
+router.post('/create', middlewares.checkAuthentication, RatingsController.createRating); // Create a new rating for a specific request
+
+router.get('/:id', middlewares.checkAuthentication, RatingsController.read); // Read a rating by request Id
+router.get('/stu/:id', middlewares.checkAuthentication, RatingsController.readStuRatings); // Read all ratings by one student
+router.get('/sen/:id', middlewares.checkAuthentication, RatingsController.readSenRatings); // Read all ratings by one senior
+
+router.put('/:id', middlewares.checkAuthentication, RatingsController.update); // Update a rating by request_id
+//router.delete('/:id', middlewares.checkAuthentication, StuOfferController.remove); // Delete a student offer by Id
+
+
+/* duplicates with checkAuthentication
+router.post('/create', middlewares.checkAuthentication, RatingsController.createRating); // Create a new rating for a specific request
+
+router.get('/:id', middlewares.checkAuthentication, RatingsController.read); // Read a rating by request Id
+router.get('/myStu/rating', middlewares.checkAuthentication, RatingsController.readStuRatings); // Read all ratings by one student
+router.get('/mySen/rating', middlewares.checkAuthentication, RatingsController.readSentRatings); // Read all ratings by one senior
+
+router.put('/:id', middlewares.checkAuthentication, RatingsController.update); // Update a rating by request_id
+*/
+
+
+module.exports = router;
